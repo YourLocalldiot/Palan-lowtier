15a40cabfe9166e1719379738c4d2bd33179427b
YourLocalldiot <Giangvu8709@gmail.com>
2026-09-19 12:22:24 +0700

    expand scope to all asian

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def predict(interpreter: tf.lite.Interpreter, batch: np.ndarray) -> np.ndarray:
 
 
 def main() -> None:
-	st.set_page_config(page_title="Palan Lowtier", page_icon="\U0001f30f")
+	st.set_page_config(page_title="Palan-lowtier", page_icon="\U0001f30f")
 	st.title("Palan Lowtier: Guess the Location")
 	st.caption("Currently trained on street-view images from across Asia.")
 
